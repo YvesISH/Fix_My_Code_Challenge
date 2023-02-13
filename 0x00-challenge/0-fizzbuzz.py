301fc3c5f7e5b8befe54f9b0d43c7f3759c45bf1
YvesISH <ivanmutware999@gmail.com>
2023-02-13 02:21:20 -0800

    FizzBuzz

---
+++ 0x00-challenge/0-fizzbuzz.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""FizzBuzz"""
+
+impor sys
+
+
+def fizzbuzz(n):
+    """ A FizzBuzz function """
+    if n < 1:
+        return
+
+    tmp_result = []
+    for i in range(1, n + 1):
+        if (i % 3) == 0 and (i % 5) == 0:
+            tmp_result.append("FizzBuzz")
+        elif (i % 3) == 0:
+            tmp_result.append("Fizz")
+        elif (i % 5) == 0:
+            tmp_result.append("Buzz")
+        else:
+            tmp_result.append(str(i))
+    print(" ".join(tmp_result))
+
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        print("Missing number")
+        print("Usage: ./0-fizzbuzz.py <number>")
+        print("Example: ./0-fizzbuzz.py 89")
+        sys.exit(1)
+
+    number = int(sys.argv[1])
+    fizzbuzz(number)
