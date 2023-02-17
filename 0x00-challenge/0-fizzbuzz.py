9aeba6b51f4c13ffe7c322bc6caad35aa2399464
YvesISH <ivanmutware999@gmail.com>
2023-02-17 00:43:16 -0800

    FizzBuzz

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-"""FizzBuzz"""
-
-impor sys
+""" FizzBuzz
+"""
+import sys
 
 
 def fizzbuzz(n):
-    """ A FizzBuzz function """
+    """
+    FizzBuzz function prints numbers from 1 to n separated by a space.
+
+    - For multiples of three print "Fizz" instead of the number and for
+      multiples of five print "Buzz".
+    - For numbers which are multiples of both three and five print "FizzBuzz".
+    """
     if n < 1:
         return
 
@@ -20,6 +26,7 @@ def fizzbuzz(n):
         else:
             tmp_result.append(str(i))
     print(" ".join(tmp_result))
+
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
