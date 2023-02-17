1ac67a2eeb3e0eae04ae51f33c9cdb60d8fd8c4d
YvesISH <ivanmutware999@gmail.com>
2023-02-17 00:44:56 -0800

    Sort

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -1,29 +1,29 @@
 ###
-#Sort integer arguments (Ascending)
 #
+#  Sort integer arguments (ascending)
 #
-### 
+###
 
 result = []
-ARVG.each do |arg|
-  # skip if not integer
-  next if arg !~ /^-?[0-9]+$/
+ARGV.each do |arg|
+    # skip if not integer
+    next if arg !~ /^-?[0-9]+$/
 
-  # convert to integer
-  i_arg = arg.to_i
+    # convert to integer
+    i_arg = arg.to_i
 
-  #insert result at the right position
-  is_inserted = false
-  i = 0 
-  l = result.size
-  while !is_inserted && i < l do
-      if result[i] < i_arg
-        i += 1
-      else
-        result.insert(i, i_arg)
-        is_inserted = true
-        break
-      end
+    # insert result at the right position
+    is_inserted = false
+    i = 0
+    l = result.size
+    while !is_inserted && i < l do
+        if result[i] < i_arg
+            i += 1
+        else
+            result.insert(i, i_arg)
+            is_inserted = true
+            break
+        end
     end
     result << i_arg if !is_inserted
 end
